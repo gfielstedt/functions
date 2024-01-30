98cfb8b9e49b2db1fd220bc2b78d083e0aac1747
Ginessa <gfielstedt@gmail.com>
2024-01-29 22:49:18 -0300

    pintar ok y css ok

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/*variables*/
+
+const onlyDiv = document.getElementById("ele1");
+
+/*funciones*/
+
+function paint(element) {
+  element.style.backgroundColor = "yellow";
+}
+
+/*eventos y llamados*/
+
+ele1.addEventListener("click", function () {
+  paint(ele1);
+});
