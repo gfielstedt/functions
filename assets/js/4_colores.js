0f19a33223d6815decc24f1148ece0b68cb0a908
Ginessa <gfielstedt@gmail.com>
2024-01-29 21:41:47 -0300

    start 4_colores

---
+++ assets/js/4_colores.js
@@ -0,0 +1,32 @@
+/*variables*/
+
+const divB = document.getElementById("blue");
+const divR = document.getElementById("red");
+const divG = document.getElementById("green");
+const divY = document.getElementById("yellow");
+
+/*funciones*/
+
+function paintBlack(element) {
+  element.style.backgroundColor = "black";
+}
+
+/*eventos y llamados*/
+
+divB.addEventListener("click", function () {
+  paintBlack(divB);
+});
+
+divB.addEventListener("click", function () {
+  paintBlack(divB);
+});
+
+divB.addEventListener("click", function () {
+  paintBlack(divB);
+});
+
+divB.addEventListener("click", function () {
+  paintBlack(divB);
+});
+
+
